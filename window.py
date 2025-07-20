377b6e0d9ef2a176ee89728b8a2d9e2e139a7a16
Guillaume M. <58004135+GuillaumeMZ@users.noreply.github.com>
2025-07-20 12:58:28 +0200

    feat: add window.dimensions

--- window.py
+++ window.py
@@ -3,6 +3,23 @@ import ctypes as ct
 
 Handle = ct.c_void_p
 
-def find_window(window_name: str) -> Handle | None:
+def find(window_name: str) -> Handle | None:
     """Finds a window by title. Returns a handle to the window if it exists, and None otherwise."""
     return ct.windll.user32.FindWindowW(None, window_name) or None
+
+def dimensions(window_handle: Handle) -> tuple[int, int]:
+    """Returns the size of a window in a tuple. The first element of the tuple is the width of the window, so the second is the height."""
+    class RECT(ct.Structure):
+        """"Mapping to the WINAPI's RECT structure."""
+        _fields_ = [
+            ('left', ct.c_ulong),
+            ('top', ct.c_ulong),
+            ('right', ct.c_ulong),
+            ('bottom', ct.c_ulong),
+        ]
+
+    rect = RECT()
+
+    ct.windll.user32.GetWindowRect(window_handle, ct.pointer(rect))
+
+    return (rect.right - rect.left, rect.bottom - rect.top)
